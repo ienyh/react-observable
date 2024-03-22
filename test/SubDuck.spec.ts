5516f7b7d4e10dbc77a9ac5034ba8fc7353efb40
chenyh <chenyh.site@gmail.com>
2024-03-22 11:44:50 +0800

    test SubDuck

---
+++ test/SubDuck.spec.ts
@@ -0,0 +1,63 @@
+import { expect, test, describe } from 'vitest'
+import { Observable } from 'rxjs'
+import { Base, PayloadAction, Runtime, Action, filterAction, reduceFromPayload } from '../src'
+
+describe('SubDuck', () => {
+  class Container extends Base {
+    get quickDucks() {
+      return {
+        ...super.quickDucks,
+        sub: Sub,
+      }
+    }
+    @Action
+    accept(action$: Observable<PayloadAction>) {
+      const { ducks } = this
+      return action$.pipe(filterAction(ducks.sub.types.SET_VALUE)).subscribe((action) => {
+        // 父 duck 可以处子 duck 的 action
+        expect(action.type).toBe(ducks.sub.types.SET_VALUE)
+      })
+    }
+  }
+  class Sub extends Base {
+    get quickTypes() {
+      enum Type {
+        SET_VALUE,
+      }
+      return {
+        ...Type,
+      }
+    }
+    get reducers() {
+      const { types } = this
+      return {
+        value: reduceFromPayload<string>(types.SET_VALUE, ''),
+      }
+    }
+    @Action
+    accept(action$: Observable<PayloadAction>) {
+      const { types } = this
+      return action$.subscribe((action) => {
+        // 子 duck 仅处理自己的 action
+        expect(action.type).toBe(types.SET_VALUE)
+      })
+    }
+  }
+
+  test.concurrent('sub', async () => {
+    const { duck } = Runtime.create(Container)
+    const { ducks, dispatch } = duck
+    dispatch({
+      type: ducks.sub.types.SET_VALUE,
+      payload: '1',
+    })
+    dispatch({
+      type: ducks.sub.types.SET_VALUE,
+      payload: '2',
+    })
+    dispatch({
+      type: ducks.sub.types.SET_VALUE,
+      payload: '3',
+    })
+  })
+})
