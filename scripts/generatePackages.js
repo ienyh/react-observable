d0dc9b092ff3827b3c7168a90efe6ef3f4658b8b
chenyh <chenyh.site@gmail.com>
2024-09-29 10:50:26 +0800

    chore: update scripts/generatePackages

--- scripts/generatePackages.js
+++ scripts/generatePackages.js
@@ -10,14 +10,19 @@ Object.keys(package.exports).forEach((key) => {
   
   const packagePath = path.join(__dirname, `../${key}/package.json`)
   const packageName = `${package.name}/${key.split('/')[key.split('/').length - 1]}`
-  const packageContent = JSON.stringify({
-    name: packageName,
-    export: {
-      '.': Object.keys(package.exports[key]).reduce((result, item) => {
+  const exportMap = Object.keys(package.exports[key]).reduce((result, item) => {
         return Object.assign(result, {
           [item]: path.join('../', package.exports[key][item])
         })
       }, {})
+  const packageContent = JSON.stringify({
+    name: packageName,
+    types: exportMap.types,
+    typings: exportMap.types,
+    main: exportMap.require,
+    module: exportMap.import,
+    export: {
+      '.': exportMap
     }
   }, null, 2)
   const packageDirPath = path.join(__dirname, `../${key}`)
